de8d57337e2afb7a8ae57a340370075f34ad01e4
AdrianFoxy <drugakadrian@gmail.com>
2023-04-24 06:33:50 +0300

    Task1Completed

--- ApacheSpark/app/dataframe.py
+++ ApacheSpark/app/dataframe.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum, max
+from pyspark.sql.functions import col, sum, max, count
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType, IntegerType
 
 # Define Spark session
-spark = SparkSession.builder.master("local").appName("Top Bike by Day").getOrCreate()
+spark = SparkSession.builder.master("local").appName("Top Routes").getOrCreate()
 
 # Define CSV schema
 schema = StructType([
@@ -27,26 +27,14 @@ df = spark.read.format("csv") \
              .schema(schema) \
              .load("resources/input/trip.csv")
 
-# Group by date and bike id, and find the sum of duration
-grouped_df = df.groupBy(col("start_date").cast("date").alias("date"), col("bike_id").alias("bike_id_grouped")) \
-               .agg(sum("duration").alias("total_duration"))
+# Top routes
+# group by start and end station, and count num of trips
+result_df_1 = df.groupBy("start_station_name", "end_station_name") \
+                        .agg(count("*").alias("trips_num")) \
+                        .orderBy("trips_num", ascending=False)
 
-# Find the max duration for each date
-max_duration_df = grouped_df.groupBy(col("date")) \
-                            .agg(max(col("total_duration")).alias("max_duration")) \
-                            .withColumnRenamed("date", "max_date")
-
-# Join with the original DataFrame to get the corresponding bike id
-result_df = max_duration_df.join(grouped_df, (max_duration_df.max_date == grouped_df.date) & (max_duration_df.max_duration == grouped_df.total_duration)) \
-                           .select(max_duration_df.max_date, grouped_df.bike_id_grouped, max_duration_df.max_duration)
-
-# Convert duration to integer
-result_df = result_df.withColumn("max_duration", result_df.max_duration.cast(IntegerType()))\
-                     .withColumnRenamed("bike_id_grouped", "bike_id")
-
-
-# Save result as CSV file
-result_df.repartition(10).write.format("csv") \
+# Save result of top routers as CSV file
+result_df_1.repartition(10).write.format("csv") \
              .option("header", True) \
              .mode("overwrite")\
              .save("resources/output")
